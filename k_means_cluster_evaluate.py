a9e85cd25789f12c2ce10a2a41d0173378e247ce
yudi <plkm357@163.com>
2023-01-12 00:30:13 +0800

    fix kmeans error

--- k_means_cluster_evaluate.py
+++ k_means_cluster_evaluate.py
@@ -3,30 +3,47 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score, silhouette_samples
+import seaborn as sns
+from sklearn import preprocessing
 
 def evaluate_k_means_cluster(cluster_column: pd.DataFrame, output_dir: str):
     cluster_num = [4,5,6,7,8,9,10,11,12,13,14,15]
     subplot_counter = 1
     sc_scores = []
-    for t in cluster_num:
-        # subplot_counter += 1
-        # plt.subplot(3, 2, subplot_counter)
-        kmeans_model = KMeans(n_clusters=t, n_init="auto").fit(cluster_column)
+    wcss = []
+    cluster_column = preprocessing.MinMaxScaler().fit_transform(cluster_column)
+    print(cluster_column)
+    for t in range(1, 16):
+    #     # subplot_counter += 1
+    #     # plt.subplot(3, 2, subplot_counter)
+    #     # kmeans_model = KMeans(n_clusters=t, n_init="auto").fit(cluster_column)
+        kmeans_model = KMeans(n_clusters = t, init = 'k-means++', random_state = 42).fit(cluster_column)
+        wcss.append(kmeans_model.inertia_)
 
         # for i,l in enumerate(kmeans_model.labels_):
         #     plt.plot(x1[i], x2[i],color=colors[l], marker=markers[l], ls='None')
             # pic_name = 'table_k_' + str(t) + '_i_' + str(i) + '.jpg'
             # plt.savefig(pic_name)
 
-        plt.xlim([0,15])
-        plt.ylim([0,15])
-        sc_score = silhouette_score(cluster_column, kmeans_model.labels_, metric='euclidean')
-        sc_scores.append(sc_score)
-        plt.title('K=%s, silhouette coefficient=%0.03f' %(t, sc_score))
+        # plt.xlim([0,15])
+        # plt.ylim([0,15])
+        # sc_score = silhouette_score(cluster_column, kmeans_model.labels_, metric='euclidean')
+        # sc_scores.append(sc_score)
+        # plt.title('K=%s, silhouette coefficient=%0.03f' %(t, sc_score))
 
-    plt.figure()
-    plt.plot(cluster_num, sc_scores, '*-')
-    plt.xlabel('Number of Clusters')
-    plt.ylabel('Silhouette coefficient Score')
+    # plt.figure()
+    # plt.plot(cluster_num, sc_scores, '*-')
+    # plt.xlabel('Number of Clusters')
+    # plt.ylabel('Silhouette coefficient Score')
+    # plt.show()
+    # plt.savefig(os.path.join(output_dir, 'k_means_sc.jpg'))
+
+
+
+    plt.figure(figsize=(10,5))
+    sns.lineplot(range(1, 16), wcss, markers = 'o', color = 'red')
+    plt.title("Elbow")
+    plt.xlabel('Number of clusters')
+    plt.ylabel('WCSS')
     plt.show()
-    plt.savefig(os.path.join(output_dir, 'k_means_sc.jpg'))
+
--- questionnaire_main.py
+++ questionnaire_main.py
@@ -9,6 +9,7 @@ from sklearn.metrics import silhouette_score, silhouette_samples
 from data_pre_processsor import pre_processs_data
 from k_means_cluster_evaluate import evaluate_k_means_cluster
 from cluster_statistics import ClusterStatistics
+import seaborn as sns
 
 pd.options.mode.chained_assignment = None
 pd.options.display.unicode.east_asian_width = True
@@ -19,23 +20,65 @@ output_dir = os.path.join(dirname, 'output')
 _original_questionnaire_ = pd.read_csv(os.path.join(dirname, 'input/questionnaire_data.csv'), encoding='gb18030')
 cluster_column, useful_column = pre_processs_data(_original_questionnaire_, output_dir)
 
+# plt.figure(figsize=(20,20), dpi=200)
+# plt.subplot(3,3,1)
+# sns.boxplot(x = 'sex', data = cluster_column)
+#
+# plt.subplot(3,3,2)
+# sns.boxplot(x = 'age', data = cluster_column)
+#
+# plt.subplot(3,3,3)
+# sns.boxplot(x = 'education', data = cluster_column)
+#
+# plt.subplot(3,3,4)
+# sns.boxplot(x = 'profession', data = cluster_column)
+#
+# plt.subplot(3,3,5)
+# sns.boxplot(x = 'annual_pay', data = cluster_column)
+#
+# plt.subplot(3,3,6)
+# sns.boxplot(x = 'family_num', data = cluster_column)
+#
+# plt.subplot(3,3,7)
+# sns.boxplot(x = 'residence_type', data = cluster_column)
+#
+# plt.subplot(3,3,8)
+# sns.boxplot(x = 'community_space', data = cluster_column)
+#
+# plt.subplot(3,3,9)
+# sns.boxplot(x = 'exercise_frequency', data = cluster_column)
+# plt.show()
+
+# corrmat = cluster_column.corr()
+# print(corrmat)
+# f, ax = plt.subplots(figsize=(12, 9))
+# sns.heatmap(corrmat, vmax=.08, square=True)
+
 print("************************************************************************")
 evaluate_k_means_cluster(cluster_column, output_dir)
 
 
-cluster_num = 6
+cluster_num = 5
 
 model = Pipeline([
     # ('BN', preprocessing.StandardScaler()),('KMS', KMeans(n_clusters=cluster_num, n_init="auto", random_state=0))
     ('BN', preprocessing.StandardScaler()),('KMS', KMeans(init="k-means++", n_clusters=cluster_num, n_init=50, random_state=420))
-])
+ ])
 model.fit(cluster_column)
 # joblib.dump(model, "./model/model.pkl")
 # model = joblib.load("./model/model.pkl")
 y_pred = model.predict(cluster_column)
 
-useful_column['label'] = y_pred
-cluster_column['label'] = y_pred
+print('result')
+print(y_pred)
+
+plt.scatter(cluster_column['education'], cluster_column['annual_pay'], c = y_pred)
+plt.xlabel('education')
+plt.ylabel('annual_pay')
+plt.show()
+
+# useful_column['label'] = y_pred
+# cluster_column['label'] = y_pred
 
 # plt.figure('用户画像聚类',figsize=(30, 15), dpi=320)
 # plt.title('radviz')
@@ -43,11 +86,17 @@ cluster_column['label'] = y_pred
 # plt.show()
 # plt.savefig(os.path.join(output_dir,'user_portrait.jpg'))
 
-print(useful_column)
+#* print(useful_column)
 # useful_column.to_excel("./useful_column.xlsx", encoding='gb18030')
 
-statistic = ClusterStatistics(useful_column, cluster_num)
-statistic.get_cluster_statistics()
+#* statistic = ClusterStatistics(useful_column, cluster_num)
+#* statistic.get_cluster_statistics()
+
+
+
+
+
+
 
 # ERROR: Still can't figure out how this method works
 # from pandas.plotting import andrews_curves
